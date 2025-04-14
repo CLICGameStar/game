654a3005bd98ef4dd639df451b46f1a98dcdc134
Clément Josso <clement.josso@epfl.ch>
2025-04-14 15:23:58 +0200

    style: update spacing

--- main/scripts/main.gd
+++ main/scripts/main.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _on_bullet_hell_pressed() -> void:
 	get_tree().change_scene_to_file("res://puzzle/scenes/puzzle.tscn")
-	
+
+
 func on_puzzle_game_pressed() -> void:
-		get_tree().change_scene_to_file("res://puzzle/scenes/puzzle.tscn")
+	get_tree().change_scene_to_file("res://puzzle/scenes/puzzle.tscn")
